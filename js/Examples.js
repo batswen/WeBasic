54a1e4decb6a7b403c75c5bcafd11a3b825baebd
batswen <batswen@gmail.com>
2021-02-25 10:39:12 +0100

    updated examples

--- js/Examples.js
+++ js/Examples.js
@@ -1,7 +1,7 @@
 const examples = {
     "Assignment": `;pi is predefined
 var a,b,a0_A,cc,xcv,aäßé_µµ\ncls\na=3<5-2\nb="a"=="a":a0_A = 3 * -5\ncc = 15 and 7:xcv = "\q\\\\\a"\naäßé_µµ=5\ndump`,
-    "Print": `cls:println "🍕" * 20 + "!"\nvar k:k=9:println "abc", 2, "rr" ,k\n\nprint "Hello\\n":println "World!"\nprintln ---3 * 2.5\n\n; cprint writes to the js console\ncprint "console.log emulation"`,
+    "Print": `cls:println "🍕" * 20 + "!"\nprintln "\\q\\\\\\a"\nvar k:k=9:println "abc", 2, "rr" ,k\n\nprint "Hello\\n":println "World!"\nprintln ---3 * 2.5\n\n; cprint writes to the js console\ncprint "console.log emulation"`,
     "IfThen1": `var a\ncls\na = 5:if a > 0 then a_cond = "Ja"\nif a == 0 then result = "true" else result = "false"\ndump\nif 0 then print "Yes":print "Always"`,
     "IfThen2": `cls\nif 0 then print "true" else print "false"\nif 1 then print "true" else print "false"\nif 0\n print "true"\nelse\n print "false"\nendif\nif 1\n print "true"\nelse\n print "false"\nendif\n`,
     "While": `var a\ncls\na=10:while a>5 do a=a-1:println a\nprintln\na=10:while a>5\n\tprintln a\n\ta = a - 0.5\nendwhile\ndump`,
