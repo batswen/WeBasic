71ceb1e19ccf7ef00de2337176bd875498cb6d36
batswen <batswen@gmail.com>
2022-02-09 14:07:05 +0100

    added jasmine

--- spec/lexer.js
+++ spec/lexer.js
@@ -1,7 +1,26 @@
-const lexer = require("./Lexer")
+// const lex = require("./Lexer")
+import Lexer from "../js/Lexer.js"
+import Token, { TokenType } from "../js/Token.js"
+
+let tokens
+
+beforeEach(() => { tokens = new Lexer("cls:print").makeTokens() })
 
 describe("Lexer", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(false);
-  });
-});
+    it("gets length wrong", function() {
+        // console.log(tokens[0])
+        expect(tokens[0].length).toBe(4)
+    })
+    it("can't tokenize CLS", () => {
+        expect(tokens[0][0].tokentype).toBe(TokenType.KEYWORD)
+        expect(tokens[0][0].value).toBe("CLS")
+    })
+    it("can't tokenize :", () => {
+        expect(tokens[0][1].tokentype).toBe(TokenType.COLON)
+        expect(tokens[0][1].value).toBe(null)
+    })
+    it("can't tokenize PRINT", () => {
+        expect(tokens[0][2].tokentype).toBe(TokenType.KEYWORD)
+        expect(tokens[0][2].value).toBe("PRINT")
+    })
+})
